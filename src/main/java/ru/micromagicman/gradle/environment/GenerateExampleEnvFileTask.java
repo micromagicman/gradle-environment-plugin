9a11522baf50219d21157ea2ffd3a4afe0acf239
micromagicman <evgen@micromagicman.ru>
2025-04-03 15:59:25 +0300

    example file creation

--- src/main/java/ru/micromagicman/gradle/environment/GenerateExampleEnvFileTask.java
+++ src/main/java/ru/micromagicman/gradle/environment/GenerateExampleEnvFileTask.java
@@ -1,12 +1,47 @@
 package ru.micromagicman.gradle.environment;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.gradle.api.DefaultTask;
+import org.gradle.api.provider.Property;
+import org.gradle.api.tasks.OutputFile;
 import org.gradle.api.tasks.TaskAction;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+@Setter
+@Getter
 public class GenerateExampleEnvFileTask extends DefaultTask {
+
+    @OutputFile
+    private File outputFile;
 
     @TaskAction
     void execute() {
-        System.out.println( "This is generate example environment file task" );
+        try {
+            System.out.println( outputFile.getAbsolutePath() );
+            if ( !createOutputFileIfDoesNotExists() ) {
+                throw new RuntimeException( "Cannot create file " + outputFile.getName() );
+            }
+            try ( final OutputStream outputStream = new FileOutputStream( outputFile ) ) {
+                outputStream.write( "".getBytes() );
+            }
+        } catch ( IOException exception ) {
+            throw new RuntimeException( exception );
+        }
+    }
+
+    private boolean createOutputFileIfDoesNotExists() throws IOException {
+        if ( outputFile.exists() ) {
+            return true;
+        }
+        final File directory = outputFile.getParentFile();
+        if ( !directory.exists() && !directory.mkdirs() ) {
+            return false;
+        }
+        return outputFile.createNewFile();
     }
 }
--- src/test/java/ru/micromagicman/gradle/environment/EnvironmentPluginTest.java
+++ src/test/java/ru/micromagicman/gradle/environment/EnvironmentPluginTest.java
@@ -1,5 +1,6 @@
 package ru.micromagicman.gradle.environment;
 
+import lombok.extern.slf4j.Slf4j;
 import org.gradle.testkit.runner.BuildResult;
 import org.gradle.testkit.runner.BuildTask;
 import org.gradle.testkit.runner.GradleRunner;
@@ -8,18 +9,23 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
-import java.util.List;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@Slf4j
 class EnvironmentPluginTest {
 
     private static final Pattern APPLICATION_OUTPUT_PATTERN =
@@ -34,7 +40,7 @@ class EnvironmentPluginTest {
 
     @Test
     void testEnvironmentPluginWithoutEnvFile() throws IOException {
-        createFile(
+        createProjectFile(
                 "build.gradle",
                 """
                         plugins {
@@ -47,7 +53,7 @@ class EnvironmentPluginTest {
                         }
                         """
         );
-        createFile(
+        createProjectFile(
                 "src/main/java/Application.java",
                 """
                         class Application {
@@ -74,7 +80,7 @@ class EnvironmentPluginTest {
 
     @Test
     void testEnvironmentPlugin() throws IOException {
-        createFile(
+        createProjectFile(
                 "build.gradle",
                 """
                         plugins {
@@ -87,7 +93,7 @@ class EnvironmentPluginTest {
                         }
                         """
         );
-        createFile(
+        createProjectFile(
                 ".env",
                 """
                         API_TOKEN=test-token
@@ -95,7 +101,7 @@ class EnvironmentPluginTest {
                         MILLION=1000000
                         """
         );
-        createFile(
+        createProjectFile(
                 "src/main/java/Application.java",
                 """
                         class Application {
@@ -122,7 +128,7 @@ class EnvironmentPluginTest {
 
     @Test
     void testCreateExampleEnvFileTask() throws IOException {
-        createFile(
+        createProjectFile(
                 "build.gradle",
                 """
                         plugins {
@@ -130,9 +136,13 @@ class EnvironmentPluginTest {
                             id 'application'
                             id 'ru.micromagicman.environment'
                         }
+                                                
+                        generateExampleEnvFile {
+                            outputFile = file("$projectDir/.env.example")
+                        }
                         """
         );
-        createFile(
+        createProjectFile(
                 ".env",
                 """
                         API_TOKEN=test-token
@@ -148,6 +158,10 @@ class EnvironmentPluginTest {
         final BuildTask task = result.task( ":generateExampleEnvFile" );
         assertNotNull( task );
         assertEquals( TaskOutcome.SUCCESS, task.getOutcome() );
+        assertProjectFileExists(
+                ".env.example",
+                ""
+        );
     }
 
     private void assertEnvironmentOutput( final String[] expectedOutput, final BuildResult result ) {
@@ -161,7 +175,7 @@ class EnvironmentPluginTest {
         assertArrayEquals( expectedOutput, environmentOutput.split( "\n" ) );
     }
 
-    private void createFile( final String fileName, final String content ) throws IOException {
+    private void createProjectFile( final String fileName, final String content ) throws IOException {
         final File buildFile = new File( projectDir, fileName );
         final File parentDir = buildFile.getParentFile();
         if ( !parentDir.exists() && !parentDir.mkdirs() ) {
@@ -169,6 +183,15 @@ class EnvironmentPluginTest {
         }
         try ( final OutputStream outputStream = new FileOutputStream( buildFile ) ) {
             outputStream.write( content.getBytes() );
+        }
+    }
+
+    private void assertProjectFileExists( final String expectedFileName, final String expectedFileContent ) throws IOException {
+        final File[] found = projectDir.listFiles( file -> Objects.equals( expectedFileName, file.getName() ) );
+        assertNotNull( found );
+        assertEquals( 1, found.length, "Expected file not found: " + expectedFileName );
+        try ( final InputStream inputStream = new FileInputStream( found[0] ) ) {
+            assertEquals( expectedFileContent, new String( inputStream.readAllBytes() ) );
         }
     }
 }
